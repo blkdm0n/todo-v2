35bab38731ff227bcd0e811aed079b55016b3dc3
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-05-29 18:19:47 -0700

    requirement #2 complete added a function to add new todo

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 More complex implementation of Version 1 incorporating functions todo
 
 Basic Requirements:
-[ ] It should have a function to display todos
-[ ] It should have a function to add new todos
+[X] It should have a function to display todos
+[X] It should have a function to add new todos
 [ ] It should have a function to change a todo
 [ ] It should have a function to delete a todo
 
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-//REQ#1 - It should have a function to display todos
+//REQ #1 - It should have a function to display todos
 
 var todos = [`Study algorithms`, `Whiteboarding Practice`, `Get a job with an awesome company`];
 
 function displayTodos() {
-  console.log(`Bernard's todos: ${todos}`);
+  console.log(`Bernard's todos:`, todos);
 }
 
-displayTodos();
+//displayTodos();
+
+//REQ #2 - It should have a function to add new todos
+
+function addTodo(newTodo) {
+  todos.push(newTodo);
+  displayTodos();
+}
+
+addTodo(`Live Happily Ever After`);
