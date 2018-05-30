047242139f5e99c55eb6580a321a73d7ce8da1eb
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-05-29 18:16:09 -0700

    complete first requirement added displayTodos function

---
+++ index.js
@@ -0,0 +1,9 @@
+//REQ#1 - It should have a function to display todos
+
+var todos = [`Study algorithms`, `Whiteboarding Practice`, `Get a job with an awesome company`];
+
+function displayTodos() {
+  console.log(`Bernard's todos: ${todos}`);
+}
+
+displayTodos();
